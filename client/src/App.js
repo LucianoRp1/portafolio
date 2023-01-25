0327954c248cb95f5ffaaf47d4533a232e70bcda
LucianoRp1 <105827151+LucianoRp1@users.noreply.github.com>
2023-01-25 00:51:05 -0300

    prueba 3

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ function App() {
       <BrowserRouter>  
     <Routes>
     <Route path='/' index element={<FirstLanding/>} />
+<Route path='/home' element={<Home />} />
       <Route path='*' element={<>No match</>}></Route>
     </Routes>
   
