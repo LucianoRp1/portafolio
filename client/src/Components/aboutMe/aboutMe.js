455ba8a7ff7d16b468e10cdda904b72e8135eb19
LucianoRp1 <105827151+LucianoRp1@users.noreply.github.com>
2023-01-24 23:13:51 -0300

    Add files via upload

---
+++ client/src/Components/aboutMe/aboutMe.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import './aboutMe.css'
+
+
+
+
+
+
+
+const AboutMe = () => {
+
+
+	return (
+        <div className="card" >
+        <div className='boxAbout'>
+            <div className='cardContainer'>
+       <p>Hello   I'm</p>
+  <section className="animation">
+    <div className="first"><div>Luciano Rodriguez</div></div>
+    <div className="second"><div>Front-End Developer</div></div>
+    <div className="third"><div>Back-End  Developer</div></div>
+  </section>
+         </div>
+         <div><p className='pAbout'>Full Stack Web Developer. Passionate about web development, creating pleasant and fully functional visual interfaces for the user.
+         Focused on learning more each time to improve my skills and grow in my development professionally and as a person, to be able to carry out what they have learned in a good work environment.</p></div>
+
+</div>
+</div>
+	)
+}
+
+
+
+export default AboutMe
---
+++ client/src/Components/project/project.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import { useEffect} from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import './project.css'
+import piVideogames from '../../Media/piVideogames.mp4'
+
+
+const Proyect = () => {
+
+	 useEffect(() => {
+        AOS.init({
+            duration: 2000,
+        })
+    }, [])
+
+	return (
+		<div className="card2" data-aos="zoom-in">
+    <div className="img-container2">
+
+        <div className="img">
+
+        <div className='containerVideo'>
+        <video src={piVideogames} autoPlay loop muted controls />
+        </div>
+        </div>
+        <div  className='descriptionProyect' data-aos="zoom-in">
+        <h1 className='h1Proyect'>Project Description:</h1><p>I come to share with you my Henry's individual project in which I have promoted, it is a Raw type page using <a className='linkApi' href='https://rawg.io/apidocs' target='-blank'>https://rawg.io/apidocs </a>(includes music 🎧) . Used technology: <br/>
+🔹 Javascript <br/>
+🔹 Front-End: Html, React, Redux, CSS puro.<br/>
+🔹 Back-End: Node.js con Express.<br/>
+🔹 Database: PostgreSQL y Sequelize.<br/>
+
+It is a Videogame App that has:<br/>
+✔ Landing Page.<br/>
+✔ Home with search engine, combined filters and orders.<br/>
+✔ Details of each video game.<br/>
+✔ Controlled form to create new video games.<br/>
+✔ controlled prev and next buttons.<br/>
+✔ Submit button disabled until the form fields are completed.<br/></p></div>
+        <div className="description card2">
+
+            <span className="title">
+                Pi-VideoGames
+            </span>
+        </div>
+
+    </div>
+</div>	)
+}
+
+
+
+export default Proyect
