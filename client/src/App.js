672d3b27c19e3992901e7129d58f16b33ee7ecd1
LucianoRp1 <105827151+LucianoRp1@users.noreply.github.com>
2023-01-25 00:52:24 -0300

    prueba 4

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route,  Routes} from "react-router-dom";
 import './App.css';
-//import Home from './Components/home/home.js'
+import Home from './Components/home/home.js'
 import FirstLanding from './Components/firstLanding/firstLanding.js'
 
 function App() {
